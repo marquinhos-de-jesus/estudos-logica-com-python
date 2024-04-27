e45c9bd6fa4e3affdda7057a93264b941f62ae04
Marcos Anunciação de Jesus <anunciacaomarcos2@gmail.com>
2024-04-27 03:38:44 +0000

    teste2

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,11 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+# Variável que armazena uma string
+texto = "Olá, mundo!"
+
+# Lista que armazena várias strings
+lista_strings = ["Python", "é", "uma", "linguagem", "de", "programação"]
+
+# Imprimindo o conteúdo da variável e da lista
+print("Conteúdo da variável texto:", texto)
+print("Conteúdo da lista lista_strings:", lista_strings)
